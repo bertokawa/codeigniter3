316d4dc5762200be2503afc9af8a8ba19996fc4e
Berto <humberto.kawa@hotmail.com>
2017-11-10 18:33:11 -0200

    API Tweets
    create an API of Social Midia

--- application/config/twitter.php
+++ application/config/twitter.php
@@ -10,8 +10,8 @@ $config['user'] = 'twitter-username';
 $config['pass'] = 'twitter-password';
 
 // required only for api v1.1 search
-$config['consumer_token']  = 'FUiAvVxYFDfFCPCwHlrW8dglg';
-$config['consumer_secret'] = 'JNEDUd9S6T8ReaWc7FtLtHvOmBwOafaCMgIwWNKdNeaQIWr2SY';
-$config['access_token']    = '916679159304343552-LfH2nmreKBwsOXCqHh8I6CbZp3blsfB';
-$config['access_secret']   = 'ipfWGjhOVcPETHdzdkARXdsEKN8gs2reGUeihisXfsSAo';
+$config['consumer_token']  = '';
+$config['consumer_secret'] = '';
+$config['access_token']    = '';
+$config['access_secret']   = '';
 ?>
